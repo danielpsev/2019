b3b88112f892f0652225ff4af52a9e8d4e3d558f
Daniel <danielpsev@gmail.com>
2022-12-05 23:39:54 +0200

    .

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title>MC.LT</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/main.css">
+        <link href="http://allfont.ru/allfont.css?fonts=prototype" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/minecraftia" type="text/css"/>
+              <link rel="stylesheet" href="animate.min.css">
+               <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+                <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init(
+    
+    );
+  </script>
+</header>
+
+<body>
+    <div class=" mySlides header-img">
+
+        <div class="logo "><a href="/index.html"><b style="color: #2e7cf0;">M</b><b>C.LT</b></a></div>
+        
+        <nav>
+            <ul>
+            
+                <li><a href="#">Pagrindinis</a></li>
+                <li><a href="#">Forumas</a></li>
+                <li><a href="#">Paslaugos</a></li>
+                <li><a href="kontaktai.html">Kontaktai</a></li>
+                <li><a href="#">D.U.K</a></li>
+                <li><a href="#">Kita</a></li>
+            
+            </ul>
+        </nav>
+                <div class="one"></div>
+        <article class="margin-auto slider">
+        <h1 class="" style="text-align: center; font-family: Prototype; color: white; font-size: 22px;"><b style="color: #2e7cf0">N</b>aujiena</h1><br>
+        <span class="slider-text mcfont">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span><br><br>
+        <div class="button-more"><a href="#">Skaityti daugiau</a></div>
+        </article>
+        
+    
+    </div>
+    
+        <div class="mc-bg "> 
+            <div class="mc-bg-2 margin-auto">
+            
+            
+            <!----------------------Naujienos pradzia--------------------------------->
+                <div class="news-container margin-auto " data-aos="flip-down">
+                    <div class="news-top"><p class="news-top-text">Naujos paslaugos</p></div>
+                        <div class="news-article news-img " style="background-image: url('img/images.jpg');">
+                        <div class="news-text ">
+                        
+                        
+                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                <div class="button-more"><a href="#">Skaityti daugiau</a></div>
+                        </span>
+                        
+                        
+                                                   
+                        </div></div>
+                    
+                    
+                    
+                        <div class="news-footer">
+                            <div class="news-footer-text news-date">Paskelbta: <span style="color: #2e7cf0;">2019-11-17 15:10</span></div>
+                            <div class="news-footer-text news-author">Autorius: <span style="color: #2e7cf0;">Litukas</span></div>
+                        </div>
+                </div>
+                
+
+            <!----------------------Naujienos pabaiga--------------------------------->
+            
+            
+            <!----------------------Naujienos pradzia--------------------------------->
+                <div class="news-container margin-auto" data-aos="flip-down">
+                    <div class="news-top"><p class="news-top-text">Naujos paslaugos</p></div>
+                        <div class="news-article news-img" style="  background-image: url('img/1.jpg');"><div class="news-text">
+                        
+                        
+                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
+                        
+                        
+                                         <div class="button-more"><a href="#">Skaityti daugiau</a></div>                  
+                        </div></div>
+                    
+                    
+                    
+                        <div class="news-footer">
+                            <div class="news-footer-text news-date">Paskelbta: <span style="color: #2e7cf0;">2019-11-17 15:10</span></div>
+                            <div class="news-footer-text news-author">Autorius: <span style="color: #2e7cf0;">Litukas</span></div>
+                        </div>
+                </div>
+                
+
+            <!----------------------Naujienos pabaiga--------------------------------->
+            
+            <!----------------------Naujienos pradzia--------------------------------->
+                <div class="news-container margin-auto " data-aos="flip-up">
+                    <div class="news-top"><p class="news-top-text">Naujos paslaugos</p></div>
+                        <div class="news-article news-img" style="  background-image: url('img/2.png');"><div class="news-text">
+                        
+                        
+                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
+                        
+                        
+                                                           <div class="button-more"><a href="#">Skaityti daugiau</a></div>                                    
+                        </div></div>
+                    
+                    
+                    
+                        <div class="news-footer">
+                            <div class="news-footer-text news-date">Paskelbta: <span style="color: #2e7cf0;">2019-11-17 15:10</span></div>
+                            <div class="news-footer-text news-author">Autorius: <span style="color: #2e7cf0;">Litukas</span></div>
+                        </div>
+                </div>
+                
+
+            <!----------------------Naujienos pabaiga--------------------------------->
+
+
+            </div>
+        
+        
+        
+        
+        </div>
+
+        <!----------<footer> <div class="footer-text"> <p>&copy 2019 Visos teises saugomos. Litukas - MCGO.LT</p></div></footer>----------------->
+                  <footer> <div class="footer-text"> <p>&copy   2019    Visos teises saugomos. Litukas - MCGO.LT</p></div></footer>
+
+
+</body>
+
+
+
+
+</html>
